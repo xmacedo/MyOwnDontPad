6a87cac9f7051cd512b2dd1f1209e40ff66d6065
Felipe Macedo <felipe.macedo@capitanigroup.com>
2025-02-02 20:19:10 -0300

    Adding schedule

--- src/main/java/com/xmacedo/MyOwnDontPad/service/TextPageWebSocketHandler.java
+++ src/main/java/com/xmacedo/MyOwnDontPad/service/TextPageWebSocketHandler.java
@@ -9,12 +9,19 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class TextPageWebSocketHandler extends TextWebSocketHandler {
     private final Map<String, String> pages = new ConcurrentHashMap<>();
     private final Map<WebSocketSession, String> sessionPageMap = new ConcurrentHashMap<>();
     private final ObjectMapper mapper = new ObjectMapper();
+    private final Map<String, ScheduledFuture<?>> tasksToUpdate = new ConcurrentHashMap<>();
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
 
 
     @Override
@@ -38,7 +45,7 @@ public class TextPageWebSocketHandler extends TextWebSocketHandler {
         if (content != null) {
             // Save or update the page content
             pages.put(pageName, content);
-            broadcastUpdate(pageName, content);
+            waitUntilUpdate(pageName, content);
         } else {
             // Retrieve the page content
             String retrievedContent = pages.getOrDefault(pageName, "");
@@ -46,7 +53,21 @@ public class TextPageWebSocketHandler extends TextWebSocketHandler {
         }
     }
 
+    private void waitUntilUpdate(String pageName, String content) {
+        ScheduledFuture<?> existingTask = tasksToUpdate.get(pageName);
+        if (existingTask != null && !existingTask.isDone()) {
+            existingTask.cancel(false); // Cancela a tarefa anterior se ainda não foi executada
+        }
+
+        ScheduledFuture<?> newTask = scheduler.schedule(() -> {
+            broadcastUpdate(pageName, content);
+        }, 500, TimeUnit.MILLISECONDS); // Delay de 500ms
+
+        tasksToUpdate.put(pageName, newTask);
+    }
+
     private void broadcastUpdate(String pageName, String content) {
+
         String updateMessage;
         try {
             updateMessage = mapper.writeValueAsString(Map.of("content", content));
